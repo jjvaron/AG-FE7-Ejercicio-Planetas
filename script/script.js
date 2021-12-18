816b9fe7907fbf3de1ede7f31af1b8fcb91f4cb4
jjvaron <j.jvaron01@gmail.com>
2021-12-18 07:26:14 -0500

    Se agregan los tamaños de los planetas, uso de la función map

--- script/script.js
+++ script/script.js
@@ -1,11 +1,29 @@
 let nombrePlanetas = Array('Mercurio','Venus','Tierra','Marte','Jupiter','Saturno','Urano','Neptuno')
 let distancia = [57,108,149,227,778,1434,2871,'67'] // Neptuno 4495
-const tamanio = []
+const tamanio = [2439,6051,6371,3389,69911,58232,25362,24662]
 
 console.log(nombrePlanetas)
 console.log(distancia)
 console.log(tamanio)
 
+nombrePlanetas.forEach((value,index,array) => {
+    console.log('Planeta', index, value)
+});
+
+nombrePlanetas.map((planetas,index)=>{
+    return(
+        console.log(`El planeta ${planetas} tiene tamaño de: ${tamanio[index]} km`)
+    )
+})
+
+// fun map()
+
+let resultadoMap = nombrePlanetas.map((planetas,index)=>{
+    return(`El planeta ${planetas} tiene tamaño de: ${tamanio[index]} km`)
+})
+console.log(resultadoMap) //  ['El planeta Mercurio tiene tamaño de: 2439 km', 'El planeta Venus tiene tamaño de: 6051 km', 'El planeta Tierra tiene tamaño de: 6371 km', 'El planeta Marte tiene tamaño de: 3389 km', 'El planeta Jupiter tiene tamaño de: 69911 km', 'El planeta Saturno tiene tamaño de: 58232 km', 'El planeta Urano tiene tamaño de: 25362 km', 'El planeta Neptuno tiene tamaño de: 24662 km']
+
+/*
 // operador de igualdad ==
 for(const key in distancia){
     if(distancia[key] == '57'){
@@ -26,7 +44,7 @@ for(const key in distancia){
     }
 }
 
-/*
+
 // tipos de ciclos 
 let i=0;
 while(i <= nombrePlanetas.length -1){
